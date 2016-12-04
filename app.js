47622f8c1b8aa67a984a0ce84e8939db813b8d7e
kevin <minghui.liu@trincoll.edu>
2016-12-04 00:23:21 -0500

    listen on port 3000 for now

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ var app = express()
 app.get('/', function(req, res) {
     res.end("What are you doing here? You shouldn't be here")
 })
+
+app.listen(3000, function() {
+    console.log('app started on port 3000')
+})
