b0f06e2481bb1500ecef0cd60ed7c4406edc548a
kevin <minghui.liu@trincoll.edu>
2016-12-04 00:29:37 -0500

    use port 80

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
     res.end("What are you doing here? You shouldn't be here")
 })
 
-app.listen(3000, function() {
-    console.log('app started on port 3000')
+app.listen(80, function() {
+    console.log('app started on port 80')
 })
