a2c8f48ad7d70859467afb3f8e39b8bdbf6671f1
kevin <minghui.liu@trincoll.edu>
2016-12-04 01:01:38 -0500

    add POST /voice and POST /sms log req.body when receive POST request

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var twilio = require('twilio');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
     res.end("What are you doing here? You shouldn't be here");
+});
+
+app.post('/voice', function(req, res) {
+    console.log('received voice request');
+    console.log(req.body);
+});
+
+app.post('/sms', function(req, res) {
+    console.log('received sms request');
+    console.log(req.body);
 });
 
 app.listen(app.get('port'), function() {
