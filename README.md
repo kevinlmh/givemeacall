eb95083a6a6576fdfbe6ba23c9b6723150e5801e
kevin <minghui.liu@trincoll.edu>
2016-12-04 00:19:14 -0500

    add a friendly welcome message to GET / add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Give Me a Call
+Send a text message to a twilio number and receive a phone Call
+Great tool to get out of an awkward situation
--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var express = require('express')
 var twilio = require('twilio')
 
+var app = express()
+
+
+app.get('/', function(req, res) {
+    res.end("What are you doing here? You shouldn't be here")
+})
